4cd4f2722e0d971775e59dd5aa34b714f1a1a6bd
Ryoga <isimaruru.1712@gmail.com>
2024-11-25 12:12:14 -0800

    done

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-$(function() {
+$(function () {
   // YOUR CODE HERE
-})
+  $("#toggleButton").click(function () {
+    $("p").toggle();
+  });
+  $("#colorButton").click(function () {
+    $("#colorDiv").css("background-color", "yellow");
+  });
+  $("#addClassButton").click(function () {
+    $("#classDiv").addClass("newClass");
+  });
+  $("#removeClassButton").click(function () {
+    $("#classDiv").removeClass("newClass");
+  });
+  $("#fadeInButton").click(function () {
+    $("#fadeDiv").fadeIn();
+  });
+  $("#fadeOutButton").click(function () {
+    $("#fadeDiv").fadeOut();
+  });
+  $("#slideUpButton").click(function () {
+    $("#slideDiv").slideUp();
+  });
+  $("#slideDownButton").click(function () {
+    $("#slideDiv").slideDown();
+  });
+});
